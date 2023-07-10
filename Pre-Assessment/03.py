7d6796d443ce3b83a2de49b6c1b14b0faa7ae489
Tarrylon Toney <90969441+TJTheDev@users.noreply.github.com>
2023-07-10 16:50:36 -0500

    Create 03.py

---
+++ Pre-Assessment/03.py
@@ -0,0 +1,28 @@
+"""
+The purpose of this lab is to introduce students to the process of creating, accessing, and generating log files using code. Use the template provided in order to write code that outputs log information with the basic configuration of level name and message, based on the information in the template.
+ 
+When you divide a number by zero, write an error message to the log.
+ 
+For Example, the following input:
+
+For Example, if the input is:
+9
+0
+ 
+The output to the console will be the following:
+ERROR: The exception that occurred is: division by zero
+ 
+Alternatively, if the input is:
+10
+2
+ 
+The output to the console will be the following:
+5.0
+ 
+Alternatively, if the input is:
+0
+20
+ 
+The output to the console will be the following:
+0.0
+"""
