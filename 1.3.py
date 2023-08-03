52c02fa9f3bc20c67f8e2ef98b0da82452d4e112
Tarrylon Toney <90969441+TJTheDev@users.noreply.github.com>
2023-08-03 12:54:13 -0500

    Create 1.3.py

---
+++ 1.3.py
@@ -0,0 +1,22 @@
+"""
+Write a program that will accept four grades and place them in a list. The sum of the 4 grades should be less or equal to 100. Each individual grade should be greater or equal to 0, but less or equal to 100. Include an assert statement that will display a message if any of these conditions are not met.
+
+Ex: If the input of the program is:
+25, 10, 15, 25
+
+the output of the program is:
+75
+
+Ex: If the input of the program is:
+25, 30, 20, 40
+
+the output of the program is:
+One of the grades is wrong
+
+Ex: If the input of the program is:
+25, 30, -1, 40
+
+the output of the program is:
+One of the grades is wrong
+
+"""
