43281879338472ba8ec8715feef3175506856004
Tarrylon Toney <90969441+TJTheDev@users.noreply.github.com>
2023-08-10 15:21:19 -0500

    Update Lab-1.7-InputValidation.py

--- Lab-1.7-InputValidation.py
+++ Lab-1.7-InputValidation.py
@@ -72,16 +72,13 @@ def calculate_age_in_hours(birth_date_str, current_date_str):
 
     # Calculate the age in days
     age_in_days = (current_date - birth_date).days
-
-    # Calculate the age in hours
-    age_in_hours = age_in_days * 24
-    return f"{age_in_hours} hours."
+    return f"{age_in_days} hours."
 
 if __name__ == '__main__':
     current_date_str = "2022-12-19"
 
     # Get user input for the date of birth
-    birth_date_str = input("Enter your date of birth (yyyy-mm-dd): ")
+    birth_date_str = input()
 
     try:
         # Call the function to calculate the age in hours and print the result
